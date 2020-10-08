bfc9c1bba859c3ff9328f0fffeb487746b66e0a4
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-10-08 10:26:37 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,4 @@ ZK_URL="${1:-zk://127.0.0.1:2181/metronome}"
 MESOS_MASTER_URL="${2:-zk://127.0.0.1:2181/mesos}"
 HTTP_PORT="${3:-9000}"
 
-if [ -z "$NOBUILD" ]; then
-  sbt stage
-fi
 LIBPROCESS_IP=127.0.0.1 ./target/universal/stage/bin/metronome -d -v -Dmetronome.framework.name=metronome-dev -Dmetronome.zk.url=$ZK_URL -Dmetronome.mesos.master.url=$MESOS_MASTER_URL -Dplay.server.http.port=$HTTP_PORT
